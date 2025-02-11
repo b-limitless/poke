b130755a951d72323c54e085c48745c3205bc666
Bharat <bharat.shah@mycomera.com>
2025-02-11 12:35:31 +0400

    adding column to handle favorite poke in user model

--- backend/src/users/user.schema.ts
+++ backend/src/users/user.schema.ts
@@ -9,6 +9,9 @@ export class User extends Document {
   @Prop({ required: true })
   password: string;
 
+  @Prop({ type: [Number], default: [], set: (v: number[]) => [...new Set(v)] }) 
+  favoritePoke: number[];
+
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
